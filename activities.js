b5f46eea67e63f737ab2de20f425786d6c1f0eb0
NiallD124 <Niallduff89@gmail.com>
2022-09-02 12:38:04 +0100

    first commit

---
+++ activities.js
@@ -0,0 +1,12 @@
+// create an object called pet
+// somecode here 
+// somecode here 
+// somecode here 
+// somecode here 
+
+
+//generate a random number 
+// somecode here 
+// somecode here 
+// somecode here 
+// somecode here 
